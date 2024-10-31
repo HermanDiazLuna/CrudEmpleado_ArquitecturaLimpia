164ad76f7001270af6a3ae4f5766ba16fde37873
Herman Diaz Luna <herosdial@hotmail.com>
2024-10-31 18:02:30 -0500

    Test unitarios actualizar y buscar todos los empleados

--- src/test/java/com/hdiaz/crud_empleados/domain/usecase/empleados/EmpleadoUseCaseTest.java
+++ src/test/java/com/hdiaz/crud_empleados/domain/usecase/empleados/EmpleadoUseCaseTest.java
@@ -10,6 +10,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class EmpleadoUseCaseTest {
 
     @Mock
@@ -21,6 +24,8 @@ class EmpleadoUseCaseTest {
     private Empleado empleado;
 
     private Empleado empleadoDB;
+
+    private List<Empleado> listaEmpleados;
 
     @BeforeEach
     public void setUp() {
@@ -36,6 +41,10 @@ class EmpleadoUseCaseTest {
                 .cedula("12345")
                 .nombre("Orlando Perez")
                 .build();
+
+        listaEmpleados = new ArrayList<>();
+
+        listaEmpleados.add(empleadoDB);
     }
 
     @Test
@@ -63,4 +72,40 @@ class EmpleadoUseCaseTest {
         Mockito.verify(empleadoGateway, Mockito.times(1)).buscarEmpleadoPorId(empleadoId);
     }
 
+    @Test
+    public void buscarTodosLosEmpleadosTest() {
+
+        Mockito.when(empleadoGateway.buscarTodos()).thenReturn(listaEmpleados);
+
+        List<Empleado> resultado = empleadoUseCase.buscarTodosLosEmpleados();
+
+        Assertions.assertNotNull(resultado);
+        Assertions.assertEquals("12345", resultado.get(0).getCedula());
+        Assertions.assertEquals("Orlando Perez", resultado.get(0).getNombre());
+
+        Mockito.verify(empleadoGateway, Mockito.times(1)).buscarTodos();
+    }
+
+    @Test
+    public void actualizarEmpleadoTest() {
+
+        Integer empleadoId = 1;
+        Empleado empleadoActualizado = Empleado.builder()
+                .nombre("Orlando Gomez")
+                .cedula("12345")
+                .build();
+
+        Mockito.when(empleadoGateway.buscarEmpleadoPorId(empleadoId)).thenReturn(empleadoDB);
+        Mockito.when(empleadoGateway.actualizarEmpleado(Mockito.any(Empleado.class)))
+                .thenReturn(empleadoActualizado);
+
+        Empleado resultado = empleadoUseCase.actualizarEmpleado(empleadoActualizado, empleadoId);
+
+        Assertions.assertNotNull(resultado);
+        Assertions.assertEquals("Orlando Gomez", resultado.getNombre());
+        Assertions.assertEquals("12345", resultado.getCedula());
+
+        Mockito.verify(empleadoGateway, Mockito.times(1)).buscarEmpleadoPorId(empleadoId);
+        Mockito.verify(empleadoGateway, Mockito.times(1)).actualizarEmpleado(Mockito.any(Empleado.class));
+    }
 }
